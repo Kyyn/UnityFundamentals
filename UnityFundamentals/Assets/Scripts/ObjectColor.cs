a5e404c0d3c45d4ad199aa77a9b92e38d8b1ebef
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-16 02:07:00 +0200

    End_Section 58

---
+++ UnityFundamentals/Assets/Scripts/ObjectColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColor : MonoBehaviour 
+{
+
+    public Color changedColor = Color.white;
+    private Color originalColor = Color.white;
+
+    private void Awake()
+    {
+        originalColor = GetComponent<Renderer>().material.color;
+    }
+    // Use this for initialization
+    void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = changedColor;
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = originalColor;
+        }
+	}
+}
---
+++ UnityFundamentals/Assets/Scripts/ObjectScaling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaling : MonoBehaviour 
+{
+    public float scaleAmount = 3.0f;
+    public Vector3 originalScale = Vector3.zero;
+
+    // Use this for initialization
+
+
+    private void Awake()
+    {
+        originalScale = this.transform.localScale;
+    }
+    void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.G))
+        {
+            //this.transform.localScale = new Vector3(scaleAmount,scaleAmount, scaleAmount);
+            this.transform.localScale = Vector3.one * scaleAmount;
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.G))
+        {
+            this.transform.localScale =originalScale;
+        }
+	}
+}
