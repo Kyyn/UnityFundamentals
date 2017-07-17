4871c0dabb043f492238e4a0057d16c2aba68099
NSN-INTRA\oscarrof <orobfer@gmail.com>
2017-07-17 11:54:36 +0200

    End_Section 65

---
+++ UnityFundamentals/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnType = null;
+    public float scaleMin = 0.2f;
+    public float scaleMax = 0.5f;
+    public float force = 100.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject obj = Instantiate(spawnType) as GameObject;
+
+            obj.transform.position = this.transform.position;
+
+            float scaleSize = Random.Range(scaleMin, scaleMax);
+
+            obj.transform.localScale = Vector3.one * scaleSize;
+
+            obj.AddComponent<Rigidbody>();
+            obj.GetComponent<Rigidbody>().AddRelativeForce(Vector3.back * force);
+
+            obj.name = "mySphere";
+        }
+	}
+}
