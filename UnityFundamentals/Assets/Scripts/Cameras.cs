e0599d756d1c2c4ce405ea81550a1021274d5323
NSN-INTRA\oscarrof <orobfer@gmail.com>
2017-07-18 10:02:29 +0200

    End_Section 73

--- UnityFundamentals/Assets/Scripts/Cameras.cs
+++ UnityFundamentals/Assets/Scripts/Cameras.cs
@@ -9,8 +9,10 @@ public class Cameras : MonoBehaviour
     public Transform target = null;
 
     public Vector3 speed = new Vector3(4.0f, 2.0f, 1.0f);
-
     public Vector3 nextPosition = Vector3.zero;
+
+    public enum CameraState { none, followPosition, lookAtPlayer, both};
+    public CameraState cameraState = CameraState.none;
 
 	// Use this for initialization
 	void Start () 
@@ -26,11 +28,27 @@ public class Cameras : MonoBehaviour
 
     private void LateUpdate()
     {
+        switch (cameraState)
+        {
+            case CameraState.none: break;
+            case CameraState.followPosition:    FollowPosition(); break;
+            case CameraState.lookAtPlayer:      LookAtPlayer(); break;
+            case CameraState.both:              FollowPosition(); LookAtPlayer(); break;
+        }
+
+
+    }
+
+    void FollowPosition()
+    {
         nextPosition.x = Mathf.Lerp(this.transform.position.x, target.position.x, speed.x * Time.deltaTime);
         nextPosition.y = Mathf.Lerp(this.transform.position.y, target.position.y, speed.y * Time.deltaTime);
         nextPosition.z = Mathf.Lerp(this.transform.position.z, target.position.z, speed.z * Time.deltaTime);
         this.transform.position = nextPosition;
-        //this.transform.position = target.position;
+    }
+
+    void LookAtPlayer()
+    {
         this.transform.LookAt(player.position);
     }
 }
