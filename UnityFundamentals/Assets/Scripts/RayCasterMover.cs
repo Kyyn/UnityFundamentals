091dd378cab5ee089e61c0b939be71db5010f0ca
NSN-INTRA\oscarrof <orobfer@gmail.com>
2017-07-19 12:22:21 +0200

    End_Section 82

---
+++ UnityFundamentals/Assets/Scripts/RayCasterMover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCasterMover : MonoBehaviour
+{
+    public Camera _camera;
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject hitObject = null;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+        {
+            ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast (ray, out hit))
+            {
+                if (hit.collider.name == "Cube")
+                {
+                    hitObject = hit.collider.gameObject;
+                    offset = hitObject.transform.position - hit.point;
+                }
+            }
+        }
+
+        if (hitObject)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ray = _camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast (ray, out hit))
+                {
+                    hitObject.transform.position = new Vector3(hit.point.x + offset.x, hitObject.transform.position.y, hit.point.z + offset.z);
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                hitObject = null;
+            }
+        }
+	}
+}
--- UnityFundamentals/Assets/Scripts/Raycaster.cs
+++ UnityFundamentals/Assets/Scripts/Raycaster.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Raycaster : MonoBehaviour 
 {
+    public Camera _camera;
+    public GameObject obj;
+    public Transform origin;
+    public float rayLenght = 2.0f;
 
+    private Ray ray;
+    private RaycastHit hit;
 	// Use this for initialization
 	void Start () 
 	{
@@ -14,6 +20,51 @@ public class Raycaster : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if (Input.GetMouseButtonDown(0))
+        {
+            //ray = _camera.ScreenPointToRay(Input.mousePosition);
+            /*if (Physics.Raycast(ray))
+            {
+                Debug.Log("Hit Something.");
+            }
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider)
+                {
+                    Debug.Log("Hit object named: " + hit.collider.name);
+                    Instantiate(obj, hit.point, hit.transform.rotation);
+                }
+            }
+
+            RaycastHit[] hits = null;
+            hits = Physics.RaycastAll(ray);
+
+            if (hits.Length > 0)
+            {
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    RaycastHit _hit = hits[i];
+                    Debug.Log("Hit: " + _hit.collider.name);
+                }
+            }
+            */
+            RaycastHit[] hits = null;
+            hits = Physics.RaycastAll(origin.position, Vector3.forward, rayLenght );
+            if (hits.Length > 0)
+            {
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    if (hits[i].collider)
+                    {
+                        Debug.Log("Hit: " + hits[i].collider.name);
+                    }
+                    
+                }
+            }
+            Debug.DrawRay(origin.position, Vector3.forward * rayLenght, Color.green, 3.0f);
+        }
+        //Debug.DrawRay(origin.position, Vector3.forward * rayLenght, Color.green, 3.0f);
+        //Debug.DrawLine(ray.origin, hit.point, Color.red);
 	}
 }
