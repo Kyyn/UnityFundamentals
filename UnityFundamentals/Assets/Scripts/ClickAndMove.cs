151840e9497b929b48bbbd0d8114b3c67fd432ce
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-19 20:15:32 +0200

    End_Section 86

---
+++ UnityFundamentals/Assets/Scripts/ClickAndMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndMove : MonoBehaviour 
+{
+
+    public Camera _camera;
+    public Transform player;
+    public Vector3 moveToPosition;
+    public float speed = 5.0f;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//if (Input.GetMouseButtonDown(0))
+        //{
+            ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast (ray, out hit))
+            {
+                if (hit.collider.name == "Plane")
+                {
+                    moveToPosition = hit.point;
+                }
+            }
+        //}
+        player.transform.position = Vector3.Lerp(player.transform.position, moveToPosition, speed * Time.deltaTime);
+	}
+}
