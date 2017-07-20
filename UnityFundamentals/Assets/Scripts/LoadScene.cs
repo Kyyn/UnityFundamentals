5336c218dfb77f41a6cb16308e24a7fbc8824e48
NSN-INTRA\oscarrof <orobfer@gmail.com>
2017-07-20 10:27:05 +0200

    End_Status END

---
+++ UnityFundamentals/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public int loadSceneId = 0;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown("s"))
+        {
+            //Application.LoadLevel(loadSceneId);
+            SceneManager.LoadScene(loadSceneId);
+        }
+	}
+}
